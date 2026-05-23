ceed478de0cedfe7490b42ad1fbd03ee67a53f23
Pixiedust3 <ccsirus@gmail.com>
2026-05-23 12:41:11 +0545

    start server integrated

--- Tcpserver.cpp
+++ Tcpserver.cpp
@@ -1,7 +1,7 @@
 #include "mainwindow.h"
 #include "Tcpserver.h"
 
-QTcpServer::QTcpServer (QObject* parent) : QObject(parent)
+HashcashServer::QTcpServer (QObject* parent) : QObject(parent)
 {
     mserver_ = new QTcpServer(this);
     connect(m_server, &QTcpServer::newConnection, this, &TcpServer::onNewConnection);
@@ -9,4 +9,12 @@ QTcpServer::QTcpServer (QObject* parent) : QObject(parent)
 QTcpServer::QTcpServer()
 {
     closed();
+}
+bool HashcashServer::startListening(uint_16 port)
+{
+    if (mserver_->listen(QHostAddress::Any, port)) {
+        emit updateUiLog(QString("[SERVER] Server listening on port %1...").arg(port));
+        return true;
+    }
+    return false;
 }
--- Tcpserver.h
+++ Tcpserver.h
@@ -9,14 +9,16 @@ Q_OBJECT
 
 public:
 
-explicit HashcashServer(QObject *parent = nullptr);
-~HashcashServer();
-bool hasPendingConnections() const
-{}
-bool isListening() const
-{}
-void QTcpServer::closed()
-{}
+    explicit HashcashServer(QObject *parent = nullptr);
+    ~HashcashServer();
+    bool startListening(uint_16 port);
+    bool stopListening();
+    bool hasPendingConnections() const;
+    bool isListening() const;
+
+private slots:
+    void onNewConnection();
+    void onConnectionDisconnected();
 private:
 
 QTcpServer* mserver_;
